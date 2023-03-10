c8ebfe0c959e9d588b04c69aeff1d8e6cca18623
MduduziBruceGunde <mduduzibrucegunde@gmail.com>
2023-03-10 08:31:46 -0800

    Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -28,7 +28,6 @@ int _atoi(char *s)
 	{
 		if (s[i] == '-')
 			++d;
-
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
@@ -41,17 +40,14 @@ int _atoi(char *s)
 			f = 1;
 
 			if (s[i + 1] < '0' || s[i + 1] > '9')
-
 				break;
 
 			f = 0;
 		}
 		i++;
 	}
-
 	if (f == 0)
 		return (0);
-
 	return (n);
 }
 /**
