b1f8a2808119b30a6cce47b40419c10196351999
MduduziBruceGunde <mduduzibrucegunde@gmail.com>
2023-04-04 10:05:54 -0700

    Created another code for 3-add_node_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,36 +1,38 @@
-#include <stdlib.h>
-#include <lists.h>
+#include "lists.h"
 
 /**
- * add_nodeint_end - Add a new node at the end of a listint_t list
- * @head: Pointer to a pointer to the head of the list
- * @n: Value to store in the new node
- *
- * Return: Address of the new element, or NULL if it failed
- */
+* add_nodeint_end - Adds a node at the end of a linked list
+* @head: pointer to the first element in the list
+* @n: data to insert in the new component
+*
+* Return: pointer to the new node, or NULL if it fails
+*/
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new_node, *current_node;
+listint_t *new_node, *temp_node;
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
+new_node = malloc(sizeof(listint_t));
 
-	new_node->n = n;
-	new_node->next = NULL;
+if (new_node == NULL)
+return (NULL);
 
-	if (*head == NULL)
-	{
-		*head = new_node;
-		return (new_node);
-	}
+new_node->n = n;
+new_node->next = NULL;
 
-	current_node = *head;
-	while (current_node->next != NULL)
-		current_node = current_node->next;
+if (*head == NULL)
+{
+*head = new_node;
 
-	current_node->next = next_node;
+return (new_node);
+}
 
-	return (new_node);
+temp_node = *head;
+
+while (temp_node->next != NULL)
+temp_node = temp_node->next;
+
+temp_node->next = new_node;
+
+return (new_node);
 }
